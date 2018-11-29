c0794fde7717298becd663c55c7a516a4bf15bbc
jsffeng <jsf_feng@sina.com>
2018-11-29 23:03:23 +1300

    comment update

--- to_do_func.c
+++ to_do_func.c
@@ -1,1 +1,11 @@
 /* Empty file */
+/* When a new function is added,cmock can be used for unit testing (with "make UT"), however, this may still break the normal product build (with "make BLD"), so need to create a stub function in this file to ensure no build error introduced to the real product */
+
+/* example */
+
+/* 
+int newfunction ()
+{
+	return 0;
+}
+*/
--- to_do_func.h
+++ to_do_func.h
@@ -1,1 +1,2 @@
 /* Empty file */
+/* Used to be used by cmock, just leave it here so that cmock can be used when any new functions need to be added */
